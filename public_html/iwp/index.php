f967b93abd8020d08b42b4c7db66a4a0583f433c
Nathan Hand <administrator@thehiddennation.com>
2013-04-24 03:11:17 +0100

    basic coin spawn system and crap

--- public_html/iwp/index.php
+++ public_html/iwp/index.php
@@ -17,11 +17,18 @@ include("./includes/header.php");
 					<div id="primary_content">
 				
 					<canvas width="1000" height="500" style="border:1px dotted;" id="game_canvas"></canvas>
-					
+					<button type="button" onclick="musicHandler()">BG audio on/off</button>
 					</div>
 					
 					<script src="./js/main.js"></script>
-					
-
+					<audio id="bg" loop>
+					<source src="./sound/bg.mp3" type="audio/mpeg">
+					</audio>
+					<audio id="levelup">
+					<source src="./sound/levelup.wav" type="audio/mpeg">
+					</audio>
+					<audio id="loosegame">
+					<source src="./sound/bg.mp3" type="audio/mpeg">
+					</audio>
 
 				<?PHP include("./includes/footer.php");?>
