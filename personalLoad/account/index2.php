ebbbdadac03852c30b9bd37afc7fb25c337483fc
Nathan Hand <administrator@thehiddennation.com>
2013-12-23 20:53:30 +0000

    adding the php

--- personalLoad/account/index2.php
+++ personalLoad/account/index2.php
@@ -1,19 +1,45 @@
-<!DOCTYPE html>
+<?PHP
+require("../config.php");
+require("../openid.php");
+$communityid = $_SESSION['steamid'];
+
+if ($_SESSION['steamid'] == ""){
+	header('Location: ' . $home . "login/" . '');
+}
+	//See if the second number in the steamid (the auth server) is 0 or 1. Odd is 1, even is 0
+	$authserver = bcsub($communityid, '76561197960265728') & 1;
+	//Get the third number of the steamid
+	$authid = (bcsub($communityid, '76561197960265728')-$authserver)/2;
+	//Concatenate the STEAM_ prefix and the first number, which is always 0, as well as colons with the other two numbers
+	$steamid = "STEAM_0:$authserver:$authid";
+	$link = file_get_contents('http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=' . $steam_api . '&steamids=' . $communityid . '&format=json');
+	$myarray = json_decode($link, true);
+	
+	$connect = mysqli_connect($host,$user,$pass,$dbname);
+	//Get our users redirect url
+	$communityid = mysqli_real_escape_string($connect, $communityid);
+	$redirect = mysqli_query($connect, "SELECT * FROM `users` WHERE `steamid` = '$communityid' LIMIT 0, 1");	
+	//If redirecturl is empty abandon redirect continue with the rest of the script
+	$playerarray = mysqli_fetch_array( $redirect);
+	$player_steamid = $playerarray['steamid'];
+	$youtube_enabled = $playerarray['youtube_enabled'];
+	$youtube_src = $playerarray['youtube_src'];
+	$redirect_enabled = $playerarray['redirect_enabled'];
+	$redirect_src = $playerarray['redirect_src'];
+?>
+
 <html lang="en">
   <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="">
-    <meta name="author" content="">
-    <link rel="shortcut icon" href="../../docs-assets/ico/favicon.png">
+    <meta name="author" content="Nathan Hand, Handy_man, www.thehiddennation.com">
 
-    <title>Starter Template for Bootstrap</title>
+    <title>PersonalLoad - Set your own loadingurl</title>
 
     <!-- Bootstrap core CSS -->
     <link href="../css/bootstrap.css" rel="stylesheet">
-
-    <!-- Custom styles for this template -->
     <link href="starter-template.css" rel="stylesheet">
 
     <!-- Just for debugging purposes. Don't actually copy this line! -->
@@ -47,36 +73,6 @@
 
       <div class="starter-template">
 	  <h1 class="hints">PersonalLoad - Set your own loading screen</h1>
-	  <!--
-        <span>
-		Youtube music :
-		</span>
-		</br>
-		<form name="input" action="update.php" method="post">
-		<select name="yt_status">
-		<option value="enabled"<?PHP if ($youtube_enabled == 1){echo "selected";}?>>enabled</option>
-		<option value="disabled" <?PHP if ($youtube_enabled == 0){echo "selected";}?>>disabled</option>
-		</select>
-		</br>
-		<span>Youtube source:</span>
-		</br>
-		<input type="text" name="yt_src" value="<?PHP if ($youtube_src != ""){echo $youtube_src;}?>"><br>
-		<label>
-		Redirect page:
-		</label>
-		</br>
-		<select id="rp_status" name="rp_status">
-		<option value="enabled" <?PHP if ($redirect_enabled == 1){echo "selected";}?>>enabled</option>
-		<option value="disabled" <?PHP if ($redirect_enabled == 0){echo "selected";}?>>disabled</option>
-		</select>
-		</br>
-		redirect page source:
-		</br>
-		<input type="text" name="rp_src" value="<?PHP if ($redirect_src != ""){echo $redirect_src;}?>">
-		</br>
-		<input type="submit" value="Submit">
-		</form>
-		!-->
 		<form role="form">
 
   <div class="form-group">
@@ -88,8 +84,8 @@
 		</select>
       </label>
 	  </br>
-    <label for="exampleInputEmail1">YouTube Source link:</label>
-    <input type="text" class="form-control" id="exampleInputEmail1" placeholder="YouTube Souce Link" name="yt_src" value="<?PHP if ($youtube_src != ""){echo $youtube_src;}?>">
+    <label for="YouTubeSource">YouTube Source link:</label>
+    <input type="text" class="form-control" id="YouTubeSource" placeholder="YouTube Souce Link" name="yt_src" value="<?PHP if ($youtube_src != ""){echo $youtube_src;}?>">
 	<span class="help-block hints">Youtube source = "aHjpOzsQ9YI", this means that youtube music being enabled with that variable as the source will have the following music video play in the background <a href ="http://www.youtube.com/watch?v=aHjpOzsQ9YI">Example</a></span>
   </div>
   <div class="form-group">
@@ -101,8 +97,8 @@
 		</select>
       </label>
 	  </br>
-    <label for="exampleInputPassword1">Redirect Source Link:</label>
-    <input type="text" class="form-control" id="exampleInputPassword1" placeholder="Redirect Source Link" value="<?PHP if ($redirect_src != ""){echo $redirect_src;}?>">
+    <label for="redirectSource">Redirect Source Link:</label>
+    <input type="text" class="form-control" id="redirectSource" placeholder="Redirect Source Link" value="<?PHP if ($redirect_src != ""){echo $redirect_src;}?>">
 	<span class="help-block hints">If your redirect source is enabled (via the above checkbox) it will overwrite the YouTube music.</span>
   </div>
   <button type="submit" class="btn btn-default">Submit</button>
