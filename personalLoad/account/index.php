0abee90eaac5722e1bbd892b3a57962e27f11af4
Nathan Hand <administrator@thehiddennation.com>
2013-12-24 00:19:02 +0000

    Adding back mobile navigation :D

--- personalLoad/account/index.php
+++ personalLoad/account/index.php
@@ -59,6 +59,12 @@ if ($_SESSION['steamid'] == ""){
     <div class="navbar navbar-inverse navbar-fixed-top" role="navigation">
       <div class="container">
         <div class="navbar-header">
+		<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
           <a class="navbar-brand" href="#">Personal Load</a>
         </div>
         <div class="collapse navbar-collapse">
